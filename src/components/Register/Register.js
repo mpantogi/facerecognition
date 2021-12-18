11ec2c659beda93bbe71bf94f260d6204cc825e1
mpantogi <48733545+mpantogi@users.noreply.github.com>
2021-12-18 15:51:55 +0200

    change signin and register fetch urls

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://infinite-sands-59165.herokuapp.com', {
+    fetch('https://infinite-sands-59165.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://infinite-sands-59165.herokuapp.com', {
+    fetch('https://infinite-sands-59165.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
